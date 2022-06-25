fad9fc8463315e14c29c5413d573571cffa00127
Clément Dreptin <cdreptin@gmail.com>
2022-06-25 13:57:03 +0200

    Added a script to download premake from GitHub on POSIX systems + made a single script for Linux and macOS

--- scripts/genprojects-linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT_ABSOLUTE_PATH=`realpath "$BASH_SOURCE"`
-SCRIPT_DIR=`dirname "$SCRIPT_ABSOLUTE_PATH"`
-ROOT_DIR=`dirname "$SCRIPT_DIR"`
-
-$ROOT_DIR/tools/premake/bin/premake5 --file=$ROOT_DIR/premake5.lua gmake2
---
+++ scripts/genprojects-posix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_ABSOLUTE_PATH=`realpath "$BASH_SOURCE"`
+SCRIPT_DIR=`dirname "$SCRIPT_ABSOLUTE_PATH"`
+ROOT_DIR=`dirname "$SCRIPT_DIR"`
+PREMAKE_EXECUTABLE_PATH=$ROOT_DIR/tools/premake/bin/premake5
+
+if [[ ! -f "$PREMAKE_EXECUTABLE_PATH" ]]; then
+    echo "Downloading premake..."
+    "$SCRIPT_DIR/download-premake-posix.sh"
+fi
+
+"$PREMAKE_EXECUTABLE_PATH" --file="$ROOT_DIR/premake5.lua" gmake2
